1003bdb44eb3ffb9ef3e2e1ee342a2a4dc767c81
Oscar Lopez <oscar.lopez@accesso.com>
2016-08-08 22:21:54 -0400

    use JsonUtil and HttpStatus

--- src/main/java/com/lopez83/spark/controller/UserController.java
+++ src/main/java/com/lopez83/spark/controller/UserController.java
@@ -5,7 +5,8 @@ import static spark.Spark.post;
 
 import java.util.List;
 
-import com.google.gson.Gson;
+import org.eclipse.jetty.http.HttpStatus;
+
 import com.lopez83.spark.model.User;
 import com.lopez83.spark.services.UserService;
 import com.lopez83.spark.utils.JsonUtil;
@@ -13,26 +14,26 @@ import com.lopez83.spark.utils.JsonUtil;
 public class UserController {
 	public UserController(final UserService userService) {
 
-		Gson gson = new Gson();
-
 		// Routes, logic defined in the service
-
 		get("/users", (req, res) -> {
 			List<User> users = userService.getAllUsers();
-			return gson.toJson(users);
+			return JsonUtil.toJson(users);
 		});
 
 		get("/users/:id", (req, res) -> {
 			User user = userService.getUser(req.params(":id"));
 			if (user != null) {
-				return gson.toJson(user);
+				return JsonUtil.toJson(user);
 			}
 
-			res.status(404);
+			res.status(HttpStatus.NOT_FOUND_404);
 			return "User not found";
 		});
 
-		post("/users", (req, res) -> userService.createUser(req.queryParams("username"), req.queryParams("email")),
+		// post?username=myusername&email=myemail
+		post("/users",
+				(req, res) -> userService.createUser(
+						req.queryParams("username"), req.queryParams("email")),
 				JsonUtil.json());
 
 	}
