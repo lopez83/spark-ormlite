c57ade00323e94a72c1568fed19d52dedcaa6ad8
Damian Rogers <damian.rogers@accesso.com>
2016-08-09 21:08:26 -0400

    Match functionality

--- src/main/java/com/accesso/challengeladder/services/MatchService.java
+++ src/main/java/com/accesso/challengeladder/services/MatchService.java
@@ -2,18 +2,29 @@ package com.accesso.challengeladder.services;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.accesso.challengeladder.model.Match;
+import com.accesso.challengeladder.model.MatchStatus;
+import com.accesso.challengeladder.model.MatchUser;
+import com.accesso.challengeladder.model.User;
 import com.accesso.challengeladder.utils.DBHelper;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import org.apache.log4j.Logger;
 import com.j256.ormlite.support.ConnectionSource;
 
 public class MatchService
 {
 	private ConnectionSource connectionSource;
 	private Dao<Match, String> matchDao;
+	private Dao<User, String> userDao;
+	private Dao<MatchUser, String> matchUserDao;
+	private Dao<MatchStatus, String> matchStatusDao;
+	
+	private static final Logger logger = Logger.getLogger(MatchService.class.getCanonicalName());
 
 	public MatchService() throws SQLException, IOException
 	{
@@ -22,11 +33,119 @@ public class MatchService
 
 		this.connectionSource = connectionSource;
 		matchDao = DaoManager.createDao(this.connectionSource, Match.class);
+		userDao = DaoManager.createDao(this.connectionSource, User.class);
+		matchUserDao = DaoManager.createDao(this.connectionSource, MatchUser.class);
+	}
+	
+	public Match getMatch(String id) {
+		Match response;
+		
+		try {
+			response = matchDao.queryForId(id);
+		}
+		catch(SQLException sqle) {
+			logger.error(sqle);
+			return null;
+		}
+		return response; 
+	}
+	
+	public List<MatchUser> getMatchUsers(String matchId) {
+		
+		List<MatchUser> response;
+		try {
+			 response = matchUserDao.queryForEq("match_id", matchId);
+		}
+		catch(SQLException sqle) {
+			logger.error(sqle);
+			return null;
+		}
+		return response;
+	}
+	
+	public Match createMatch(ArrayList<String> users) {
+		if(users == null) return null;
+		
+		// create an entry in the match table first to get an id
+		Match newMatch = new Match();
+		
+		newMatch.setCreationTimestamp(new Date());
+		newMatch.setVictorUser(null);
+		try {
+			newMatch.setCreatorUser(userDao.queryForId(users.get(0)));
+		}
+		catch(SQLException sqle) {
+			logger.error(sqle);
+			return null;
+		}
+		// check on what the match statuses will be
+		// newMatch.setStatusId(0);
+		newMatch.setMatchTimestamp(null);
+		
+		try {
+			 matchDao.create(newMatch);
+		}
+		catch(SQLException sqle) {
+			logger.error(sqle);
+			return null;
+		}
+		
+		// iterate over users, write an entry to match_user for each
+		for(String userId:users) {
+			MatchUser tempMatchUser = createMatchUser(userId, Integer.toString(newMatch.getId()));
+			if(tempMatchUser == null) {
+				logger.error("Unable to create a MatchUser for userId " + userId);
+				return null;
+			}
+		}
+		
+		return newMatch;
+	}
+	
+	public Match updateMatch(String matchId, String victorId, Date matchTimestamp, String status, Date creationTimestamp, String creatorId) {
+		// all values will be optional (except matchId), pass in null to make no change
+		
+		Match match = getMatch(matchId);
+		
+		if(match == null) return null;
+		try {
+			if(victorId != null) match.setVictorUser(userDao.queryForId(victorId));
+			
+			if(matchTimestamp != null) match.setMatchTimestamp(matchTimestamp);
+			
+			if(status != null) match.setMatchStatus(matchStatusDao.queryForId(status));
+			
+			if(creationTimestamp != null) match.setCreationTimestamp(creationTimestamp);
+			
+			if(creatorId != null) match.setCreatorUser(userDao.queryForId(creatorId));
+		}
+		catch(SQLException sqle) {
+			logger.error(sqle);
+			return null;
+		}
+		return match;
 	}
 
 	public List<Match> getAllMatches() throws SQLException
 	{
 		List<Match> matchList = matchDao.queryForAll();
 		return matchList;
+	}
+	
+	private MatchUser createMatchUser(String userId, String matchId) {
+		
+		MatchUser newMatchUser = new MatchUser();
+		
+		try {
+			newMatchUser.setUser(userDao.queryForId(userId));
+			newMatchUser.setMatch(matchDao.queryForId(matchId));
+			matchUserDao.create(newMatchUser);
+		}
+		catch(SQLException sqle) {
+			logger.error(sqle);
+			return null;
+		}
+		
+		return newMatchUser; 
 	}
 }
