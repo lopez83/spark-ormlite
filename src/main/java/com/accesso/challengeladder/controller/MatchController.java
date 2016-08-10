a64ade027defd45b71fb2d5184721d02fdfb9323
Oscar Lopez <oscar.lopez@accesso.com>
2016-08-10 15:40:14 -0400

    - pom updated to generate jar with dependencies, including properties - TODO comments addded

--- src/main/java/com/accesso/challengeladder/controller/MatchController.java
+++ src/main/java/com/accesso/challengeladder/controller/MatchController.java
@@ -23,31 +23,30 @@ public class MatchController
 			List<Match> rankings = matchService.getAllMatches();
 			return JsonUtil.toJson(rankings);
 		});
-		
+
 		// get information about a match
 		get("/matches/:id", (req, res) -> {
 			return JsonUtil.toJson(matchService.getMatch(req.params(":id")));
 		});
-		
+
 		// get all users in a match
 		get("/matches/:id/users", (req, res) -> {
 			return JsonUtil.toJson(matchService.getMatchUsers(req.params(":id")));
 		});
-		
+
+		// TODO create match functionality
 		post("/matches", (req, res) -> {
-			
+
 			return null;
 		});
-		
-		put("/matches/:id", (req, res) -> {
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			return JsonUtil.toJson(matchService.updateMatch(
-					req.queryParams("matchId"),
-					req.queryParams("victorId"),
-					sdf.parse(req.queryParams("matchTimestamp")),
-					req.queryParams("status"),
-					sdf.parse(req.queryParams("creationTimestamp")),
-					req.queryParams("creatorId")));
-		});
+
+		// TODO params are passed as a json string in the body entity
+		put("/matches/:id",
+				(req, res) -> {
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+					return JsonUtil.toJson(matchService.updateMatch(req.queryParams("matchId"), req.queryParams("victorId"),
+							sdf.parse(req.queryParams("matchTimestamp")), req.queryParams("status"), sdf.parse(req.queryParams("creationTimestamp")),
+							req.queryParams("creatorId")));
+				});
 	}
 }
--- src/main/java/com/accesso/challengeladder/utils/DBHelper.java
+++ src/main/java/com/accesso/challengeladder/utils/DBHelper.java
@@ -2,6 +2,7 @@ package com.accesso.challengeladder.utils;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
@@ -16,17 +17,17 @@ public class DBHelper
 	public DBHelper() throws IOException
 	{
 
-		// Properties prop = new Properties();
-		// prop.load(DBHelper.class.getClassLoader().getResourceAsStream("db.properties"));
+		Properties prop = new Properties();
+		prop.load(DBHelper.class.getClassLoader().getResourceAsStream("db.properties"));
 
 		// TODO - fix these later
-		this.databaseUrl = "jdbc:mysql://192.168.2.246/accesso_table_tennis";
-		this.userDb = "dba";
-		this.passDb = "sql";
+		// this.databaseUrl = "jdbc:mysql://192.168.2.246/accesso_table_tennis";
+		// this.userDb = "dba";
+		// this.passDb = "sql";
 
-		// this.databaseUrl = prop.getProperty("db.url");
-		// this.userDb = prop.getProperty("db.user");
-		// this.passDb = prop.getProperty("db.password");
+		this.databaseUrl = prop.getProperty("db.url");
+		this.userDb = prop.getProperty("db.user");
+		this.passDb = prop.getProperty("db.password");
 	}
 
 	public ConnectionSource getConnectionSource() throws SQLException
